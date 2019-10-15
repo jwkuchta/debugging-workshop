8e3de42d886df080194d80f84b884e0f7fb576b0
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-15 13:28:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
-        "Accept": "application/json"
+        "Accept": "application/json",
       }
     })
-    .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
   }
 
   form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    event.preventDefault()
+    const username = document.getElementById('name-input').value
+    if(username !== "") {
+      fetchJoke()
+      .then(resp => resp.json())
+      .then(jokeData => {
+        let joke = jokeData.joke
+        let newJokeLi = document.createElement('li')
+        newJokeLi.innerHTML = `<span class="username">${username} says:</span> ${joke}`
+        jokeList.appendChild(newJokeLi)
+      })
+    } else {
+      alert("You must enter a name")
+    }
   })
 })
